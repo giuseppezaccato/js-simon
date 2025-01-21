6329c37314fecc9c6a20d0072f2a70f9e29bf797
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-21 22:22:35 +0100

    ADD:  eventlistener + ultimi task

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// **Descrizione:**
-// Visualizzare in pagina 5 numeri casuali.Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente,
-// nell'ordine che preferisce.
-// Dopo che sono stati inseriti i 5 numeri,
-// il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-// **NOTA**: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
 
 // **BONUS:**
 // - Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
@@ -18,41 +11,86 @@ const answers = document.getElementById("answers-form");
 
 //task riempio numArray con 5 numeri random
 for (let i = 0; i < 5; i++) {
-    numArray.push(Math.floor(Math.random() * 50) + 1);
-    console.log(numArray)
+    numArray.push(random(1, 50));
+    console.log(numArray);
 }
 console.log(numArray)
 // numList.createElement('li') //se funziona questo è MEGLIO del console.log
 
 // //task  scrivo i 5 numeri come < li > nella < ul > numList ripescandoli dall'array!
+// Visualizzare in pagina 5 numeri casuali
 for (let i = 0; i < numArray.length; i++) {
     const listItem = document.createElement('li');
     listItem.textContent = numArray[i];
     numList.appendChild(listItem);
 }
 
-//! non serve necessariamente!
-// function random(min, max) {
-//     Math.floor(Math.random() * max) + min
-// }
+//* randomizer
+function random(min, max) {
+    return Math.floor(Math.random() * max) + min;
+}
 
-//task imposto countdown
+//task imposto countdown 
 const contoAllaRovescia = setInterval(decremento, 1000);
-let counter = 5; //fix RICORDA DI FARLO TORNARE 30 A FINE ESERCIZIO
+
+let counter = 30; //fix RICORDA DI FARLO TORNARE 30 A FINE ESERCIZIO!
+
+// Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente,
+
 function decremento() {
 
-    if (counter == 0) {
-        console.log("ho raggiunto lo zero")
+    if (counter === 0) {
+        console.log("blocco il countdown");
         clearInterval(contoAllaRovescia);
         countdown.classList.add("d-none");
         answers.classList.remove("d-none");
         numList.classList.add("d-none");
     } else {
-        console.log("sto eseguendo il decremento")
-        countdown.innerHTML = counter--
+        console.log("countdown in azione");
+        countdown.innerHTML = counter--;
     }
     return counter
 }
+//!---ATTENZIONE--- LA RACCOLTA DEI DATI VA FATTA DOPO L'EVENTLISTENER, 
+//! E QUINDI DENTRO ALLA FUNZIONE EVOCATA ALLO SCATURIRE DEL CLICK!!!
+// Dopo che sono stati inseriti i 5 numeri,
+// il software dice quanti e quali dei numeri 
+// da indovinare sono stati individuati.
+
+//task dichiaro bottone e array vuoto degli input
+const btn = document.querySelector('button');
+const inputNumbers = []; //?  è necessario?
+
+//task raccolgo i dati inseriti nei 5 input e ne creo un array finalNumbers
+const inputForm = document.querySelectorAll('.form-control');
+const finalNumbers = [];
+
+//task risultati output
+const result = document.getElementById('message');
+
+//* EVENTLISTENER
+btn.addEventListener('click', function (event) {
+
+    event.preventDefault();
+
+    for (i = 0; i < inputForm.length; i++) {
+        inputNumbers.push((inputForm[i]).value); //?  è necessario?
+        if (inputNumbers[i].includes(numArray[i])) {
+            finalNumbers.push(numArray[i]);
+        }
+    }
+    console.log(finalNumbers);
+
+    if (finalNumbers.length > 0) {
+        result.innerHTML = `Complimenti, hai indovinato: ${finalNumbers.length} numeri : ${finalNumbers.join('-')}`;
+    } else {
+        result.innerHTML = "Peccato Nessun numero indovinato.";
+    }
+});
+
+
+
+
 
 
 
